c594a8ceb854361e76c68bfe2fe6637886bf5e30
YuryVolchkov <yuravolchkov@gmail.com>
2024-07-28 12:58:15 +0300

    first Commit

---
+++ StaticLibv2/StaticLibGreeter.cpp
@@ -0,0 +1,7 @@
+﻿#include "StaticLibGreeter.h"
+#include <iostream>
+
+    void Greeter::Show_Greetings()
+    {
+        std::cout << "Здравствуйте, " << Greeting_Name << "!"<<std::endl;
+    }
---
+++ StaticLibv2/StaticLibGreeter.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <iostream>
+
+class Greeter
+{
+private:
+    std::string Greeting_Name;
+
+public:
+    Greeter(std::string Entering_Name) : Greeting_Name(Entering_Name) {}
+
+    void Show_Greetings();
+};
